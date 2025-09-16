e4ba67427a73f0222f821b4dcbd4a2bf3b86d3a4
Adarsh Shaw <dev.adarsh279@gmail.com>
2025-09-16 14:13:37 +0530

    Logout

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const userModel = require("./models/user");
 const postModel = require("./models/post");
 const cookieParser = require("cookie-parser");
 const bcrypt = require("bcrypt");
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -20,23 +20,23 @@ app.post("/register", async (req, res) => {
   let user = await userModel.findOne({ email });
   if (user) return res.send(500).send("User already registered");
 
-    bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(password, salt, async (err, hash) => {
-        let user = await userModel.create({
-          name,
-          username,
-          email,
-          password: hash,
-        });
-
-        let token = jwt.sign({ email: email, userid: user._id }, "postly");
-        res.cookie("token", token);
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, async (err, hash) => {
+      let user = await userModel.create({
+        name,
+        username,
+        email,
+        password: hash,
       });
+
+      let token = jwt.sign({ email: email, userid: user._id }, "postly");
+      res.cookie("token", token);
     });
+  });
 });
 
 app.get("/login", async (req, res) => {
-  res.render("login")
+  res.render("login");
 });
 
 app.post("/login", async (req, res) => {
@@ -44,10 +44,18 @@ app.post("/login", async (req, res) => {
   let user = await userModel.findOne({ email });
   if (!user) return res.send(500).send("Something went wrong");
 
-    bcrypt.compare(password, user.password, (err, result) => {
-        if (result) res.status(200).send("You can login")
-        else res.redirect("/login")
+  bcrypt.compare(password, user.password, (err, result) => {
+    if (result) {
+      let token = jwt.sign({ email: email, userid: user._id }, "postly");
+      res.cookie("token", token);
+      res.status(200).send("You can login");
+    } else res.redirect("/login");
   });
+});
+
+app.get("/logout", async (req, res) => {
+  res.cookie("token", "");
+  res.redirect("/login");
 });
 
 app.listen(3000, () => {
